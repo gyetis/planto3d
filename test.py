d99518d9e72bee3fbfde6b17c872ad91c1f77072
Gizem <gizemyetis93@gmail.com>
2019-01-24 11:09:27 +0300

    resize update

--- test.py
+++ test.py
@@ -5,9 +5,48 @@ from skimage import img_as_ubyte
 import numpy as np
 import cv2
 
-def testGenerator(test_path,num_image,target_size = (864,608)):
+blank_image = np.zeros((target + (3,)), np.uint8)
+blank_image[:] = (255,255,255)
+
+def resize_proportional(source_img, target_size, inter = cv2.INTER_CUBIC):
+    (source_h, source_w) = source_img.shape[:2]
+    (target_h, target_w) = target_size
+
+    if target_h < target_w and source_h < source_w:
+        r = float(target_w) / float(source_w)
+        dim = (target_w, int(source_h * r))
+    
+    if target_h < target_w and source_h > source_w:
+        rot_img = cv2.rotate(source_img, rotateCode=cv2.ROTATE_90_CLOCKWISE)
+        r = float(target_w) / float(rot_img.shape[1])
+        dim = (target_w, int(rot_img.shape[0] * r))
+    
+    if target_h > target_w and source_h < source_w:
+        rot_img = cv2.rotate(source_img, rotateCode=cv2.ROTATE_90_CLOCKWISE)
+        r = float(target_h) / float(rot_img.shape[0])
+        dim = (int(rot_img.shape[1] * r), target_h)
+    
+    if target_h > target_w and source_h > source_w:
+        r = float(target_h) / float(source_h)
+        dim = (int(source_w * r), target_h)
+    
+    resized = cv2.resize(source_img, dim, interpolation = inter)
+
+    return resized
+
+def blend_images(fore, back):
+
+    (fore_h, fore_w) = fore.shape[:2]
+
+    blend = cv2.addWeighted(fore, 1, back[0:fore_h,0:fore_w], 0, 0, back)
+    back[0:fore_h,0:fore_w] = blend
+    return back
+
+def testGenerator(test_path,num_image,target_size):
     if num_image == 1:
         img = cv2.imread(test_path+"/1.png", 0)
+        img = resize_proportional(img, target_size)
+        img = blend_images(img, blank_image)
         img = img / 255
         img = cv2.resize(img, (0,0), fx=target_size[0]/img.shape[0], fy=target_size[1]/img.shape[1])
         img = np.reshape(img,img.shape+(1,))
@@ -17,6 +56,8 @@ def testGenerator(test_path,num_image,target_size = (864,608)):
     else:    
         for i in range(num_image):
             img = cv2.imread(test_path+"/%d.png"%(i+2), 0)
+            img = resize_proportional(img, target_size)
+            img = blend_images(img, blank_image)
             img = img / 255
             img = cv2.resize(img, (0,0), fx=target_size[0]/img.shape[0], fy=target_size[1]/img.shape[1])
             img = np.reshape(img,img.shape+(1,))
@@ -32,13 +73,15 @@ def saveResult(save_path,npyfile):
 def test():
         test_path = "./uploads"
         num_img = 4
+        target = (304,432)
         model = load_model("./models/unet_ElevationData_epoch10_stepperepoch500.hdf5", custom_objects={'tf':tf})
-        testGene = testGenerator(test_path, num_image=num_img, target_size=(304,432))
+        testGene = testGenerator(test_path, num_image=num_img, target_size=target)
         results = model.predict_generator(testGene, num_img)
         saveResult("./elevs_predict",results)
 
         num_img = 1
+        target = (432,304)
         model = load_model("./models/unet_FloorPlanData_epoch50_stepperepoch500.hdf5", custom_objects={'tf':tf})
-        testGene = testGenerator(test_path, num_image=num_img, target_size=(432,304))
+        testGene = testGenerator(test_path, num_image=num_img, target_size=target)
         results = model.predict_generator(testGene, num_img)
         saveResult("./plan_predict",results)
