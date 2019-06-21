cdc7bf40be24b72f028135057e5b74f6e5c6b92e
Gizem Yetis <gizemyetis93@gmail.com>
2019-06-21 14:36:49 +0300

    Update test.py

--- test.py
+++ test.py
@@ -8,11 +8,13 @@ from resize import *
 from morph import *
 from regen import *
 
-def testGenerator(test_path,num_image,target_size, offset_height, offset_width, blank):
+def testGenerator(test_path,num_image,target_size, offset_height, offset_width):
     if num_image == 1:
         img = cv2.imread(test_path+"/1.png")
         img = resize_proportional(img, target_size, offset_height, offset_width)
-        img = blend_images(img, blank)
+        blank_image = np.zeros((target_size + (3,)), np.uint8)
+        blank_image[:] = (255,255,255)
+        img = blend_images(img, blank_image)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         img = img / 255
         img = np.reshape(img,img.shape+(1,))
@@ -23,7 +25,9 @@ def testGenerator(test_path,num_image,target_size, offset_height, offset_width, 
         for i in range(num_image):
             img = cv2.imread(test_path+"/%d.png"%(i+2))
             img = resize_proportional(img, target_size, offset_height, offset_width)
-            img = blend_images(img, blank)
+            blank_image = np.zeros((target_size + (3,)), np.uint8)
+            blank_image[:] = (255,255,255)
+            img = blend_images(img, blank_image)
             img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             img = img / 255
             img = np.reshape(img,img.shape+(1,))
@@ -40,20 +44,16 @@ def test():
         test_path = "./uploads"
         num_img = 4
         target = (256,512)
-        blank_image = np.zeros((target + (3,)), np.uint8)
-        blank_image[:] = (255,255,255)
         model = load_model("./models/elevation.hdf5", custom_objects={'tf':tf})
         testGene = testGenerator(test_path, num_image=num_img, target_size=target, \
-                                offset_height=target[0]/2, offset_width=target[1]/2, blank=blank_image)
+                                offset_height=target[0]/2, offset_width=target[1]/2)
         results = model.predict_generator(testGene, num_img)
         saveResult("./elev_predict",results)
 
         num_img = 1
         target = (512,256)
-        blank_image = np.zeros((target + (3,)), np.uint8)
-        blank_image[:] = (255,255,255)
         model = load_model("./models/floorplan.hdf5", custom_objects={'tf':tf})
         testGene = testGenerator(test_path, num_image=num_img, target_size=target, \
-                                offset_height=target[0]/5, offset_width=target[1]/5, blank=blank_image)
+                                offset_height=target[0]/5, offset_width=target[1]/5)
         results = model.predict_generator(testGene, num_img)
         saveResult("./plan_predict",results)
